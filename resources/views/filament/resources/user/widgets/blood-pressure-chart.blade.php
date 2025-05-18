243c766d16a4be7e6f5f6919fe9e988853887b0b
mehdi gandomi <coderguy1999@gmail.com>
2025-05-18 19:17:01 +0330

    responsive

--- resources/views/filament/resources/user/widgets/blood-pressure-chart.blade.php
+++ resources/views/filament/resources/user/widgets/blood-pressure-chart.blade.php
@@ -1,31 +1,31 @@
 {{-- resources/views/filament/widgets/bp-chart-widget.blade.php --}}
 
 <x-filament::widget>
-    <div class="p-4">
-        <h2 class="text-lg font-bold mb-4">میانگین فشار خون هفته جاری</h2>
+    <div class="p-2 sm:p-4">
+        <h2 class="text-base sm:text-lg font-bold mb-4">میانگین فشار خون هفته جاری</h2>
 
         {{-- Blood Pressure Data Table --}}
-        <div class="mb-4 overflow-x-auto">
-            <table class="w-full text-sm text-right">
+        <div class="mb-4 overflow-x-auto rounded-lg border border-gray-200 bg-white shadow-sm">
+            <table class="min-w-[400px] w-full text-xs sm:text-sm text-right">
                 <thead>
                     <tr class="border-b">
-                        <th class="px-4 py-2">تاریخ</th>
-                        <th class="px-4 py-2">سیستولیک</th>
-                        <th class="px-4 py-2">دیاستولیک</th>
-                        <th class="px-4 py-2">ضربان قلب</th>
+                        <th class="px-2 sm:px-4 py-2">تاریخ</th>
+                        <th class="px-2 sm:px-4 py-2">سیستولیک</th>
+                        <th class="px-2 sm:px-4 py-2">دیاستولیک</th>
+                        <th class="px-2 sm:px-4 py-2">ضربان قلب</th>
                     </tr>
                 </thead>
                 <tbody>
                     @forelse($bloodPressures as $bp)
                         <tr class="border-b">
-                            <td class="px-4 py-2">{{ \Carbon\Carbon::parse($bp->date)->format('Y/m/d H:i') }}</td>
-                            <td class="px-4 py-2">{{ $bp->avg_systolic }}</td>
-                            <td class="px-4 py-2">{{ $bp->avg_diastolic }}</td>
-                            <td class="px-4 py-2">{{ $bp->avg_heart_rate }}</td>
+                            <td class="px-2 sm:px-4 py-2">{{ \Carbon\Carbon::parse($bp->date)->format('Y/m/d H:i') }}</td>
+                            <td class="px-2 sm:px-4 py-2">{{ $bp->avg_systolic }}</td>
+                            <td class="px-2 sm:px-4 py-2">{{ $bp->avg_diastolic }}</td>
+                            <td class="px-2 sm:px-4 py-2">{{ $bp->avg_heart_rate }}</td>
                         </tr>
                     @empty
                         <tr>
-                            <td colspan="4" class="px-4 py-2 text-center">اطلاعاتی ثبت نشده است</td>
+                            <td colspan="4" class="px-2 sm:px-4 py-2 text-center">اطلاعاتی ثبت نشده است</td>
                         </tr>
                     @endforelse
                 </tbody>
@@ -34,6 +34,7 @@
 
         {{-- Chart --}}
         <div
+            class="w-full max-w-full sm:max-w-2xl mx-auto"
             x-data="{}"
             x-init="
                 setTimeout(() => {
@@ -70,6 +71,7 @@
                         },
                         options: {
                             responsive: true,
+                            maintainAspectRatio: false,
                             plugins: {
                                 legend: { position: 'top' },
                                 title: { display: true, text: 'میانگین فشار خون هفته جاری' }
@@ -86,7 +88,9 @@
                 }, 100);
             "
         >
-            <canvas id="bpChart-{{ $this->getId() }}" height="120"></canvas>
+            <div class="relative w-full" style="height:220px;">
+                <canvas id="bpChart-{{ $this->getId() }}" class="w-full h-full" style="max-width:100%;"></canvas>
+            </div>
         </div>
     </div>
 </x-filament::widget>
